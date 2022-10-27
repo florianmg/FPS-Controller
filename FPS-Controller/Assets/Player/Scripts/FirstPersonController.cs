70c310dd858d92200e3afb35ed5b62e86b9af029
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-10-27 19:07:25 +0200

    feat: basic movements for body and camera

---
+++ FPS-Controller/Assets/Player/Scripts/FirstPersonController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public bool CanMove { get; private set; } = true;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float gravity = 30.0f;
+
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 100)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 100)] private float lowerLookLimit = 80.0f;
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerCamera = GetComponentInChildren<Camera>();    
+        characterController = GetComponent<CharacterController>();  
+        Cursor.lockState = CursorLockMode.Locked;   
+        Cursor.visible = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CanMove)
+        {
+            HandleMovementInput();
+            HandleMouseLook();
+
+            ApplyFinalMovements();
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0.0f, 0.0f);
+        transform.rotation *= Quaternion.Euler(0.0f, Input.GetAxis("Mouse X") * lookSpeedX, 0.0f);
+    }
+
+    private void ApplyFinalMovements()
+    {
+        if(characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
